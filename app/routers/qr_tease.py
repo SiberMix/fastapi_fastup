5e056fd27a43c90e6df4b946af936bb63c60d758
pimonov.m <LPOI159nm>
2023-09-01 15:00:20 +0700

    fx

--- app/routers/qr_tease.py
+++ app/routers/qr_tease.py
@@ -8,19 +8,23 @@ from starlette.requests import Request
 
 from app.config.settings import qr_link_1c
 from app.routers.callback import telegram_bot
+from app.service.telegram_sender import TelegramBot
 
 router_system = APIRouter()
 
 
-async def one_c_redirect(message: str) -> int:
+telegram = TelegramBot(os.getenv("BOT_TOKEN"))
+def one_c_redirect(message: str) -> int:
     try:
         r = requests.post(url=qr_link_1c, json=json.loads(message))
-        await telegram_bot.send_message(chat_id=os.getenv('CHAT_ID'), message_text=message)
+
         if r.status_code in [201]:
+            telegram_bot.send_message(os.getenv("CHAT_ID"), 'Успешно отработан чек')
+
             return 200
         return r.status_code
     except requests.ConnectionError as e:
-        await telegram_bot.send_message(chat_id=os.getenv('CHAT_ID'), message_text=e)
+        telegram_bot.send_message(os.getenv("CHAT_ID"), e)
         return 418
 
 
@@ -33,12 +37,12 @@ def take_message(data: bytes) -> str:
 
 
 @router_system.post('/')
-async def post_info(request: Request):
+def post_info(request: Request):
     """
     Отправить ответ на 1с и получить итог
     :return:
     """
-    body = await request.body()
+    body = request.body()
 
     message = take_message(body)
 
--- app/service/telegram_sender.py
+++ app/service/telegram_sender.py
@@ -1,12 +1,16 @@
-import asyncio
-
-from telegram import Bot
+import requests
 
 
 class TelegramBot:
     def __init__(self, bot_token):
         self.bot_token = bot_token
-        self.bot = Bot(token=self.bot_token)
+        self.base_url = f"https://api.telegram.org/bot{bot_token}"
 
-    async def send_message(self, chat_id, message_text):
-        await self.bot.send_message(chat_id=chat_id, text=message_text)
+    def send_message(self, chat_id, message):
+        url = f"{self.base_url}/sendMessage"
+        data = {
+            "chat_id": chat_id,
+            "text": message
+        }
+        response = requests.post(url, data=data)
+        return response
